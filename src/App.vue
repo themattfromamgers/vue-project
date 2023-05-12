5ab475b7d67bafb2f7bfb4639e6f70587cec5dc1
themattfromamgers <133354306+themattfromamgers@users.noreply.github.com>
2023-05-12 13:13:28 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+<script setup>
+var myObject = new Vue({
+    el: '#app',
+    message: 'xd'
+})
+</script>
+
+<template>
+<main>
+  <h1>{{message}}</h1>
+  
+</main>
+</template>
+
+<style scoped>
+
+</style>
